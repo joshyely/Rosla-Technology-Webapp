9bb44025ffc3aef0916d4d1febd632e556f9b011
Joshua Ely <joshy.ely@outlook.com>
2025-03-28 16:11:21 +0000

    Added vite proxy for backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,6 +15,18 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    watch: {
+      // Watcher options
+    },
+    proxy: {
+      '/api': {
+        target: 'http://backend-dev:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
 })
